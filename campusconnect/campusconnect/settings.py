a02a86acbb210d66954a480c5ce0892e92d72385
JoshGonzalez <56143006+JoshGonzalez@users.noreply.github.com>
2023-05-02 13:49:49 -0700

    Update settings.py

--- campusconnect/campusconnect/settings.py
+++ campusconnect/campusconnect/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d$_%0k2jxgu#shc$5h3jpvm&1=p3_#1@ie3v(csbtfo86xy09g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
