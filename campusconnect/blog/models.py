3b59cd0de859129a563a295639cc8c8b655fa391
JoshGonzalez <OriginallyJoshy@gmail.com>
2023-05-02 13:27:40 -0700

    deploying...

--- campusconnect/blog/models.py
+++ campusconnect/blog/models.py
@@ -1,7 +1,27 @@
 # Create your models here.
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    avatar = models.ImageField(default='default.jpg', upload_to='profile_images')
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.user.username
+    
 class SearchLog(models.Model):
     url = models.CharField(max_length=255)
     iswp = models.BooleanField(default=False)
     search_time = models.DateTimeField(auto_now=True)
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(auto_now=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- campusconnect/blog/urls.py
+++ campusconnect/blog/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from .import views
+from .views import RegisterView
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('chat/', views.chat, name='chatroom'),
     path('profile/', views.profile, name='profile'),
+    path('register/', RegisterView.as_view(), name='users-register'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
